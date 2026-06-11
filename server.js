2711d520b8cdf4ba318ccd397d7653ca502ea494
PiCab-LP <lpinedo@trez.pe>
2026-06-11 15:53:02 -0500

    feat: 1 minute response on tierlock api to solver wise gang and lucky duck lounge

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post('/api/exportar', async (req, res) => {
                         start_date: start,
                         end_date: end,
                         status: 'all'
-                    }, { timeout: 15000 });
+                    }, { timeout: 60000 });
 
                     if (response.data.success) {
                         const transacciones = response.data.data;
